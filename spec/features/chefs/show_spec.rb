a2cf16aa309b372881ad01590c1b2698c040b27b
Brandon Gray <brandonjoe88@hotmail.com>
2023-01-09 14:42:01 -0700

    test: us2 chef show page

---
+++ spec/features/chefs/show_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'chefs show page' do
+  before(:each) do
+    @chef_1 = Chef.create!(name: "Wolfgang")
+    @chef_2 = Chef.create!(name: "Ming Sae")
+    @dish_1 = Dish.create!(name: "Lobster Bisque", description: "Bold saucy ocean insect", chef: @chef_1)
+    @dish_2 = Dish.create!(name: "Ham Sandwich", description: "Hard adn stale", chef: @chef_1)
+    @dish_3 = Dish.create!(name: "Cheese Pizza", description: "Could be a frisby", chef: @chef_2)
+    @dish_4 = Dish.create!(name: "Quesidilla", description: "Decent", chef: @chef_1)
+    @ingredient_lobster = Ingredient.create!(name: "Lobster", calories: 500)
+    @ingredient_soupbase = Ingredient.create!(name: "Soup", calories: 1000)
+    @ingredient_cheese = Ingredient.create!(name: "Cheese", calories: 1500)
+    @ingredient_ham = Ingredient.create!(name: "Ham", calories: 2000)
+    @ingredient_bread = Ingredient.create!(name: "Bread", calories: 1000)
+    @ingredient_tortilla = Ingredient.create!(name: "Tortilla", calories: 800)
+
+    @dish_1.ingredients << @ingredient_lobster
+    @dish_1.ingredients << @ingredient_soupbase
+    @dish_2.ingredients << @ingredient_bread
+    @dish_2.ingredients << @ingredient_ham
+    @dish_3.ingredients << @ingredient_bread
+    @dish_3.ingredients << @ingredient_cheese
+    @dish_4.ingredients << @ingredient_tortilla
+    @dish_4.ingredients << @ingredient_cheese
+  end
+
+  describe 'As a visitor, show page, lists chef\’s name' do
+    it 'shows a list of dishes' do
+      visit chef_path(@chef_1)
+
+      expect(page).to have_content(@chef_1.name)
+      expect(page).to have_content(@dish_1.name)
+      expect(page).to have_content(@dish_2.name)
+      expect(page).to have_content(@dish_4.name)
+    end
+  end
+end
