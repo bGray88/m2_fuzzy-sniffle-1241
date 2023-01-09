ac298ccea0cb1617e753af46f9ce3ce47d3fb422
Brandon Gray <brandonjoe88@hotmail.com>
2023-01-09 14:02:03 -0700

    feat: relationship ingredient

--- db/migrate/20230109204817_create_ingredients.rb
+++ db/migrate/20230109204817_create_ingredients.rb
@@ -3,6 +3,7 @@ class CreateIngredients < ActiveRecord::Migration[5.2]
     create_table :ingredients do |t|
       t.string :name
       t.integer :calories
+      t.references :dish, foreign_key: true
 
       t.timestamps
     end
