3e44d40530950613915d806aac466bea28985a74
Brandon Gray <brandonjoe88@hotmail.com>
2023-01-09 14:56:46 -0700

    feat: form to add dish to chef

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -2,4 +2,12 @@ class ChefsController < ApplicationController
   def show
     @chef = Chef.find(params[:id])
   end
+
+  def update
+    @chef = Chef.find(params[:id])
+    @dish = Dish.find(params[:dish_id])
+    @chef.dishes << @dish
+
+    redirect_to chef_path(@chef)
+  end
 end
